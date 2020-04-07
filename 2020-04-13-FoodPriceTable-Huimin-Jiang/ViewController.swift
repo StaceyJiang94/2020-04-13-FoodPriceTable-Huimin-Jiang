050ec1a0c1618d5c65beb69c4ba9db300a594f1c
Huimin Jiang <hjiang7@mail.sfsu.edu>
2020-04-06 20:26:15 -0700

    Initial Commit

---
+++ 2020-04-13-FoodPriceTable-Huimin-Jiang/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2020-04-13-FoodPriceTable-Huimin-Jiang
+//
+//  Created by Huimin Jiang on 4/6/20.
+//  Copyright © 2020 Huimin Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
