0c1cd2d519faa3a7eb5b68af2f5176ee7dded119
Huimin Jiang <hjiang7@mail.sfsu.edu>
2020-04-06 20:55:59 -0700

    Extra Credit - Table View

--- 2020-04-13-FoodPriceTable-Huimin-Jiang/ViewController.swift
+++ 2020-04-13-FoodPriceTable-Huimin-Jiang/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // connect tableView with swift
+    @IBOutlet weak var tableView: UITableView!
+    
+    // setup data to be serves into your table view
+    let foodNameArray = ["Roasted Chicken", "Classic Burger", "Chicken Noodle Soup", "Bow-Tie Pasta", "Pepperoni Pizza", "Italian Salad", "Grilled Steak"]
+        
+    let foodPriceArray = [13.99, 13.99, 11.99, 12.99, 11.99, 10.99, 15.99]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        // define datasource and delegate for tableview
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return number of rows in section
+        return foodPriceArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        //serve data to the cell
+        cell.textLabel?.text = foodNameArray[indexPath.row]
+        cell.detailTextLabel?.text = "$\(foodPriceArray[indexPath.row])"
+        //return the cell
+        return cell
     }
 
-
 }
+
+
 
